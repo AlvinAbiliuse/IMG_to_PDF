c09983ed8b97328731ad512eddfe750d8dab8466
Alvin Abiliuse <alvinabiliuse@gmail.com>
2020-09-10 00:39:20 +0400

    save images in chunks TODO: make it merge

--- IMG_to_PDF.py
+++ IMG_to_PDF.py
@@ -10,34 +10,57 @@ def extractFiles():
 	for manga in os.listdir('./Convert'):
 		if manga.endswith('.zip'):
 			print('Extracting %s...' % '.'.join(manga.split('.')[:-1]))
-			zipfile.ZipFile('./Convert/%s' % manga, 'r').extractall('./Convert/%s' % ('.'.join(manga.split('.')[:-1])))
+			zipfile.ZipFile('./Convert/%s' % manga, 'r').extractall(
+				'./Convert/%s' % ('.'.join(manga.split('.')[:-1])))
 			send2trash.send2trash('./Convert/%s' % manga)
 
 def writePDF():
-
+	global finalList
 	# Looping Folders
 	for manga in os.listdir('./Convert'):
 		if os.path.isdir('./Convert/%s' % manga):
 			print('Converting %s...' % manga)
-
-			# All the pages except the first page needs to be added to a list
-			# to be set to append pages when saving
-			firstPage = 0
+			# All the pages except the first page needs to be added to 
+			# a list to be set to append pages when saving
 			Pages = natsorted(os.listdir('./Convert/%s' % manga))
 			finalList = []
+			number = 0
+			chunkNum = 0
 			# Looping Images
+			print(Pages)
 			for pages in Pages:
 				try:
-					fileBase = '.'.join(pages.split('.')[:-1])
-					image = Image.open('./Convert/%s/%s' % (manga, pages))
+					print(pages)
+					image = Image.open('./Convert/%s/%s' % 
+									(manga, pages))
 					cPages = image.convert('RGB')
 					finalList.append(cPages)
+					number += 1
 				except:
 					pass
-			finalList[0].save('./Converted/%s.pdf' % manga, save_all=True, append_images=finalList[1:])
-			send2trash.send2trash('./Convert/%s' % manga)
+				if number != 5:
+					pass
+				else:
+					chunkNum += 1
+					print('\n\n')
+					# print(finalList)
+					finalList[0].save('./Convert/%s.%s.pdf' % (chunkNum, manga), 
+						save_all=True, append_images=finalList[1:])
+					finalList = []
+					number = 0
+				if pages == Pages[-1]:
+					print('\n\n')
+					chunkNum += 1
+                    # print(finalList)
+					finalList[0].save('./Convert/%s.%s.pdf' % (chunkNum, manga),
+						save_all=True, append_images=finalList[1:])
+					finalList = []
+					number = 0
+					
+			# send2trash.send2trash('./Convert/%s' % manga)
 		else:
 			print('%s is not a Zip File or Directory.' % manga)
+
 
 if __name__ == "__main__":
 	extractFiles()
