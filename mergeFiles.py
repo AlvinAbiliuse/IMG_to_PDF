e48b114aa404da13eb784855e393c5a45b0456e9
Alvin Abiliuse <alvinabiliuse@gmail.com>
2020-12-30 03:55:19 +0400

    mergeFiles as a module

---
+++ mergeFiles.py
@@ -0,0 +1,12 @@
+from dependence import *
+
+# merges the multiple pdf chunks created with writePDF() to ./Converted
+def mergeFiles(path, destination,  manga, number):
+    mergeObject = PyPDF2.PdfFileMerger()
+
+    for i in range(number):
+        mergeObject.append(PyPDF2.PdfFileReader('./%s/%s.%s.pdf' %
+                                            (path, i+1, manga), 'rb'))
+        send2trash('./%s/%s.%s.pdf' % (path, i+1, manga))
+    mergeObject.write('%s/%s.pdf' % (destination, manga))
+
