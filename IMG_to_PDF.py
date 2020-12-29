1aa68f903892d011c4c8a906378e3fe33bd18802
Alvin Abiliuse <alvinabiliuse@gmail.com>
2020-12-29 17:58:58 +0400

    splitting IMG_to_PDF.py into smaller manageable modules

--- IMG_to_PDF.py
+++ IMG_to_PDF.py
@@ -1,24 +1,27 @@
 #! /usr/bin/python3
 # IMG_to_PDF.py - extracts zip files and converts images to pdf
 
-import os
-from natsort import natsorted
-from send2trash import send2trash
-from PIL import Image
-import zipfile
-import PyPDF2
-import sys
+#import os
+#from natsort import natsorted
+#from send2trash import send2trash
+#from PIL import Image
+#import zipfile
+#import PyPDF2
+#import sys
 
-def extractFiles(path):
+from dependancies import *
+import extractFiles
 
-	# Extracts the zip file into current directory 
-	for manga in os.listdir(path):
-		if manga.endswith('.zip'):
-			print('Extracting %s...' % '.'.join(manga.split('.')[:-1]))
-			zipfile.ZipFile('./%s/%s' % 
-							(path, manga), 'r').extractall('./%s/%s' % 
-							(path, ('.'.join(manga.split('.')[:-1]))))
-			send2trash('./%s/%s' % (path, manga))
+# def extractFiles(path):
+#
+#	# Extracts the zip file into current directory 
+#	for manga in os.listdir(path):
+#		if manga.endswith('.zip'):
+#			print('Extracting %s...' % '.'.join(manga.split('.')[:-1]))
+#			zipfile.ZipFile('./%s/%s' % 
+#							(path, manga), 'r').extractall('./%s/%s' % 
+#							(path, ('.'.join(manga.split('.')[:-1]))))
+#			send2trash('./%s/%s' % (path, manga))
 
 
 
@@ -138,5 +141,5 @@ if __name__ == "__main__":
 		exit()
 	os.makedirs('./Converted', exist_ok=True)
 	convertMultipleFiles(sys.argv[1], './Converted')
-	extractFiles(sys.argv[1])
+	extractFiles.extractFiles(sys.argv[1])
 	writePDF(sys.argv[1], './Converted')
