ff8ee52dd9b8630afbebcf6aefd33e602ca07f05
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-03 03:20:39 +0400

    Changed spaces to tabs in convertMultipleFiles.py

--- convertMultipleFiles.py
+++ convertMultipleFiles.py
@@ -1,32 +1,31 @@
 from dependence import *
 from writePDF import *
 from extractFiles import *
+
 def convertMultipleFiles(path, destination):
-
-    # checks if files inside folder in path is a zip file or a folder
-    # and sets multiFIle to 1 if it is.
-    for folders in os.listdir(path):
-        multiFile = 0
-        if os.path.isdir('%s/%s' % (path, folders)):
-            for files in os.listdir('%s/%s' % (path, folders)):
-                if files.endswith('.zip'):
-                    multiFile = 1
-                elif os.path.isdir('./%s/%s/%s' %
-                                    (path, folders, files)):
-                    multiFile = 1
-            # if multifile is 1, the funtion extracts and converts
-            # files and moves it to appropriate folder after mkdir
-            if multiFile == 1:
-                print('\nWorking on %s: ' % folders)
-                try:
-                    extractFiles('%s/%s' % (path, folders))
-                except IsADirectoryError:
-                    pass
-                os.makedirs('%s/%s' % (destination, folders),
-                            exist_ok=True)
-                writePDF('%s/%s' % (path, folders), '%s/%s' %
-                                                (destination, folders))
-                send2trash('%s/%s' % (path, folders))
-                multiFile = 0
-                print('')
-
+	# checks if files inside folder in path is a zip file or a folder
+	# and sets multiFIle to 1 if it is.
+	for folders in os.listdir(path):
+		multiFile = 0
+		if os.path.isdir('%s/%s' % (path, folders)):
+			for files in os.listdir('%s/%s' % (path, folders)):
+				if files.endswith('.zip'):
+					multiFile = 1
+				elif os.path.isdir('./%s/%s/%s' %
+									(path, folders, files)):
+					multiFile = 1
+			# if multifile is 1, the funtion extracts and converts
+			# files and moves it to appropriate folder after mkdir
+			if multiFile == 1:
+				print('\nWorking on %s: ' % folders)
+				try:
+					extractFiles('%s/%s' % (path, folders))
+				except IsADirectoryError:
+					pass
+				os.makedirs('%s/%s' % (destination, folders),
+										exist_ok=True)
+				writePDF('%s/%s' % (path, folders), '%s/%s' %
+										(destination, folders))
+				send2trash('%s/%s' % (path, folders))
+				multiFile = 0
+				print('')
