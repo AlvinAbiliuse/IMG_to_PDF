7a6baecb98d8a1e3956b97621685a5ff03d74082
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-04 05:14:33 +0400

    spaces to tabs mergeFiles.py

--- mergeFiles.py
+++ mergeFiles.py
@@ -2,11 +2,10 @@ from dependence import *
 
 # merges the multiple pdf chunks created with writePDF() to ./Converted
 def mergeFiles(path, destination,  manga, number):
-    mergeObject = PyPDF2.PdfFileMerger()
+	mergeObject = PyPDF2.PdfFileMerger()
 
-    for i in range(number):
-        mergeObject.append(PyPDF2.PdfFileReader('./%s/%s.%s.pdf' %
-                                            (path, i+1, manga), 'rb'))
-        send2trash('./%s/%s.%s.pdf' % (path, i+1, manga))
-    mergeObject.write('%s/%s.pdf' % (destination, manga))
-
+	for i in range(number):
+		mergeObject.append(PyPDF2.PdfFileReader('./%s/%s.%s.pdf' %
+							(path, i+1, manga), 'rb'))
+		send2trash('./%s/%s.%s.pdf' % (path, i+1, manga))
+	mergeObject.write('%s/%s.pdf' % (destination, manga))
