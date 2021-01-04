5e41c32f0ea404b938c3fbc1fafc8d32e824ddd3
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-04 05:14:13 +0400

    space to tabs extractFiles

--- extractFiles.py
+++ extractFiles.py
@@ -2,12 +2,11 @@ from dependence import *
 
 def extractFiles(path):
 
-    # Extracts the zip file into current directory 
-    for manga in os.listdir(path):
-        if manga.endswith('.zip'):
-            print('Extracting %s...' % '.'.join(manga.split('.')[:-1]))
-            zipfile.ZipFile('./%s/%s' %
-                            (path, manga), 'r').extractall('./%s/%s' %
-                            (path, ('.'.join(manga.split('.')[:-1]))))
-            send2trash('./%s/%s' % (path, manga))
-
+	# Extracts the zip file into current directory 
+	for manga in os.listdir(path):
+		if manga.endswith('.zip'):
+			print('Extracting %s...' % '.'.join(manga.split('.')[:-1]))
+					zipfile.ZipFile('./%s/%s' %
+					(path, manga), 'r').extractall('./%s/%s' %
+					(path, ('.'.join(manga.split('.')[:-1]))))
+			send2trash('./%s/%s' % (path, manga))
