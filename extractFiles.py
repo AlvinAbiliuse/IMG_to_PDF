a6ca0c407edf2f6bb5db16acaa049bec64ad8d56
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-05 17:25:13 +0400

    IndentationError in extractFiles

--- extractFiles.py
+++ extractFiles.py
@@ -6,8 +6,8 @@ def extractFiles(path):
 	for manga in os.listdir(path):
 		if manga.endswith('.zip'):
 			print('Extracting %s...' % '.'.join(manga.split('.')[:-1]))
-					zipfile.ZipFile('./%s/%s' %
-					(path, manga), 'r').extractall('./%s/%s' %
-					(path, ('.'.join(manga.split('.')[:-1]))))
+			zipfile.ZipFile('./%s/%s' %
+							(path, manga), 'r').extractall('./%s/%s' %
+							(path, ('.'.join(manga.split('.')[:-1]))))
 			# deletes the zip files after extracting
 			send2trash('./%s/%s' % (path, manga))
