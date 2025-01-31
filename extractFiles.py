1b6dd5f39fb1a7f62ad89d32d0f51bceba3fc58f
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-01 02:21:33 +0400

    fixed extractFiles

--- extractFiles.py
+++ extractFiles.py
@@ -2,12 +2,13 @@ from dependence import *
 
 def extractFiles(path):
 
-	# Extracts the zip file into current directory 
-	for folders in os.listdir(path):
-		if folders.endswith('.zip'):
-			name = '.'.join(folders.split('.')[:-1    ]))
-			print(f'Extracting {name}...')
-			zipfile.ZipFile(f'./{path}{folders}', 'r').extractall(
-				f'./{path}/{name}')
-			# deletes the zip files after extracting
-			send2trash(f'./{path}/{folder}')
+    # Extracts the zip file into current directory 
+    for folders in os.listdir(path):
+        if folders.endswith('.zip'):
+            print(folders)
+            name = '.'.join(folders.split('.')[:-1    ])
+            print(f'Extracting {name}...')
+            zipfile.ZipFile(f'./{path}{folders}', 'r').extractall(
+                f'./{path}/{name}')
+            # deletes the zip files after extracting
+            send2trash(f'./{path}/{folder}')
