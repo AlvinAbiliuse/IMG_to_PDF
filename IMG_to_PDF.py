44eca99e9a7598033b3f68c5672d6e632dc4aea7
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-01 07:15:25 +0400

    changed sys.argv

--- IMG_to_PDF.py
+++ IMG_to_PDF.py
@@ -14,12 +14,13 @@ if __name__ == "__main__":
         print('Usage: ./IMG_to_PDF.py path')
         sys.exit()
     # exits program if entered argument is not a folder
-    if os.path.isdir(sys.argv[1]) != True:
-        print(sys.argv[1] + ' is not a folder!')
+    if os.path.isdir(sys.argv[-1]) != True:
+        print(sys.argv[-1] + ' is not a folder!')
         sys.exit()
-    if sys.argv[2] == "--clean" or sys.argv[2] == "-c":
-        cleanPath(sys.argv[1])
+    if len(sys.argv) > 2:
+        if sys.argv[1] == "--clean" or sys.argv[2] == "-c":
+            cleanPath(sys.argv[-1])
     os.makedirs('./Converted', exist_ok=True)
-    convertMultipleFiles(sys.argv[1], './Converted')
-    extractFiles(sys.argv[1])
-    writePDF(sys.argv[1], './Converted')
+    convertMultipleFiles(sys.argv[-1], './Converted')
+    extractFiles(sys.argv[-1])
+    writePDF(sys.argv[-1], './Converted')
