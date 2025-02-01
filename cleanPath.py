032f5672746f50589a16dee04bbde8af0e73675f
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-01 07:01:24 +0400

    bugfix cleanPath.py

--- cleanPath.py
+++ cleanPath.py
@@ -10,29 +10,31 @@ def cleanPath(path):
     test = path.split("-");
     print(f"Cleaning {path} \n")
     for i in os.listdir(path):
-        if i.split(".")[-1] == "html":
-            send2trash.send2trash(f"./CC/{i}")
-        elif os.path.isdir(f"{path}/{i}") == True:
-            cleanPath(f"{path}/{i}")
-        elif len(test) > 1 and test[-1] == " Asura Scans_files":
-            for i in os.listdir(path):
-                if i == "google.webp" or i == "logo.webp":
-                    send2trash.send2trash(f"{path}/{i}")
-                elif i.split(".")[-1] == "webp":
-                    if len(i.split("-")[0]) > 5:
+        try:
+            if i.split(".")[-1] == "html":
+                send2trash.send2trash(f"./{path}/{i}")
+            elif os.path.isdir(f"{path}/{i}") == True:
+                cleanPath(f"{path}/{i}")
+            elif len(test) > 1 and test[-1] == " Asura Scans_files":
+                for i in os.listdir(path):
+                    if i == "google.webp" or i == "logo.webp":
                         send2trash.send2trash(f"{path}/{i}")
-                else:
-                    send2trash.send2trash(f"{path}/{i}")
-        else:
-            for i in os.listdir(path):
-                if i.split(".")[-1] in ["js", "css"]:
-                    send2trash.send2trash(f"{path}/{i}")
-                elif i == "logo-chap.png":
-                    send2trash.send2trash(f"{path}/{i}")
-                elif len(i.split("-")) > 1:
-                    continue
-                else:
-                    send2trash.send2trash(f"{path}/{i}")
-
+                    elif i.split(".")[-1] == "webp":
+                        if len(i.split("-")[0]) > 5:
+                            send2trash.send2trash(f"{path}/{i}")
+                    else:
+                        send2trash.send2trash(f"{path}/{i}")
+            else:
+                for i in os.listdir(path):
+                    if i.split(".")[-1] in ["js", "css"]:
+                        send2trash.send2trash(f"{path}/{i}")
+                    elif i == "logo-chap.png":
+                        send2trash.send2trash(f"{path}/{i}")
+                    elif len(i.split("-")) > 1:
+                        continue
+                    else:
+                        send2trash.send2trash(f"{path}/{i}")
+        except OSError:
+            pass
 if __name__ == "__main__":
-    cleanPath(f"./CC/{i}")
+    cleanPath("./CC")
