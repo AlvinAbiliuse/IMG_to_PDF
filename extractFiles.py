612a39591b77f297724e94432073789e3a5d6e41
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-15 01:26:04 +0400

    renaming variables in extractFiles.py

--- extractFiles.py
+++ extractFiles.py
@@ -3,11 +3,11 @@ from dependence import *
 def extractFiles(path):
 
 	# Extracts the zip file into current directory 
-	for manga in os.listdir(path):
-		if manga.endswith('.zip'):
-			print('Extracting %s...' % '.'.join(manga.split('.')[:-1]))
+	for folder in os.listdir(path):
+		if folder.endswith('.zip'):
+			print('Extracting %s...' % '.'.join(folder.split('.')[:-1]))
 			zipfile.ZipFile('./%s/%s' %
-							(path, manga), 'r').extractall('./%s/%s' %
-							(path, ('.'.join(manga.split('.')[:-1]))))
+							(path, folder), 'r').extractall('./%s/%s' %
+							(path, ('.'.join(folder.split('.')[:-1]))))
 			# deletes the zip files after extracting
-			send2trash('./%s/%s' % (path, manga))
+			send2trash('./%s/%s' % (path, folder))
