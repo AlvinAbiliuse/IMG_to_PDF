fcd9e278ecd2a1a494bbc67a533af222c48c03ee
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-04-02 08:28:52 +0400

    mergeFiles.py

--- mergeFiles.py
+++ mergeFiles.py
@@ -2,10 +2,10 @@ from dependence import *
 
 # merges the multiple pdf chunks created with writePDF() to ./Converted
 def mergeFiles(path, destination, folder, number):
-	mergeObject = PyPDF2.PdfFileMerger()
+	mergeObject = PyPDF2.PdfMerger()
 
 	for i in range(number):
-		mergeObject.append(PyPDF2.PdfFileReader('./%s/%s.%s.pdf' %
+		mergeObject.append(PyPDF2.PdfReader('./%s/%s.%s.pdf' %
 							(path, i+1, folder), 'rb'))
 		os.remove('./%s/%s.%s.pdf' % (path, i+1, folder))
 	mergeObject.write('%s/%s.pdf' % (destination, folder))
