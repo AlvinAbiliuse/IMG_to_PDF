a8e84534faf74c7f0d42c9c3157c68cb937a5896
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-04-11 15:28:04 +0400

    extractFiles.py

--- extractFiles.py
+++ extractFiles.py
@@ -1,13 +1,19 @@
+#! /env/bin/python3
+
 from dependence import *
 
 def extractFiles(path):
 
 	# Extracts the zip file into current directory 
 	for folder in os.listdir(path):
-		if folder.endswith('.zip'):
+		if folder.endswith('.zip') or folder.endswith(".cbz"):
 			print('Extracting %s...' % '.'.join(folder.split('.')[:-1]))
 			zipfile.ZipFile('./%s/%s' %
 							(path, folder), 'r').extractall('./%s/%s' %
 							(path, ('.'.join(folder.split('.')[:-1]))))
 			# deletes the zip files after extracting
 			send2trash('./%s/%s' % (path, folder))
+
+
+if __name__ == "__main__":
+    extractFIles("./CC")
