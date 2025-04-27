a5d360a806d1990beebc7b8b24cb50857bef9204
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-04-27 06:06:20 +0400

    script.py

--- script.py
+++ script.py
@@ -5,10 +5,13 @@ import sys
 
 curr = os.listdir(sys.argv[1])
 curr.sort()
-print("\n".join(curr))
 
 for i in curr:
-    first = i.split(" Chapter")[0]
-    print(f"Moving {i} to {first}")
-    shutil.move(f"{sys.argv[1]}/{i}", 
-                f"{sys.argv[1]}/{first}/{i}")
+    if i.endswith(".pdf"):
+        print(i)
+        first = i.split(" Chapter")[0]
+        if os.path.isdir(f"./{sys.argv[1]}/{first}") == False:
+            os.makedirs(f"./{sys.argv[1]}/{first}");
+        print(f"Moving {i} to {first}")
+        shutil.move(f"./{sys.argv[1]}/{i}", 
+                f"./{sys.argv[1]}/{first}/{i}")
