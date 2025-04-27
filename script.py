2daf9cfb2da7741975033f4a9c4489ad50c8ae55
AlvinAbiliuse <alvinabiliuse@gmail.com>
2025-04-27 06:13:14 +0400

    script.py removed print statements

--- script.py
+++ script.py
@@ -10,10 +10,8 @@ curr.sort()
 
 for i in curr:
     if i.endswith(".pdf"):
-        print(i)
         first = i.split(" Chapter")[0]
         if os.path.isdir(f"./{sys.argv[1]}/{first}") == False:
             os.makedirs(f"./{sys.argv[1]}/{first}");
-        print(f"Moving {i} to {first}")
         shutil.move(f"./{sys.argv[1]}/{i}", 
                 f"./{sys.argv[1]}/{first}/{i}")
